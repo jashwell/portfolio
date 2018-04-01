ed4f0bed5012a3cabf0445f4da4a68d67099dba1
Jocelyn Ashwell <jocelynashwell@gmail.com>
2018-04-01 08:08:45 -0400

    Header & Footer Beginnings

---
+++ wp-content/themes/portfolio/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Jocelyn Ashwell | Developer, PHP, Ruby on Rails, WordPress</title>
+  </head>
+  <body>
+    <header>
+      Main Nav
+    </header>
+    <main>
+      Hero
+      <h1>h1</h1>
