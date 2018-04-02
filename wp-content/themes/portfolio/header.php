4ac327f78c57dbb34d9908c99b8b74d23004130c
Jocelyn Ashwell <jocelynashwell@gmail.com>
2018-04-02 07:09:02 -0400

    A little front end

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -2,11 +2,27 @@
 <html>
   <head>
     <title>Jocelyn Ashwell | Developer, PHP, Ruby on Rails, WordPress</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
   </head>
   <body>
     <header>
-      Main Nav
+      <div class="header">
+        <div class="logo">
+          Logo
+        </div>
+        <div class="main-nav">
+          <ul class="list-nav">
+            <li><a href="#"></a>About</li>
+            <li><a href="#"></a>Musings</li>
+            <li><a href="#"></a>Contact</li>
+          </ul>
+        </div>
+      </div>
     </header>
     <main>
-      Hero
       <h1>h1</h1>
+      <article>
+        <h2>Welcome To My Website</h2>
+        <p>Morbi bibendum nulla luctus varius finibus. Cras ultrices, elit non scelerisque scelerisque, lectus tortor malesuada sem, vitae mattis tellus magna id massa. Mauris a vestibulum mauris. Duis tincidunt ultricies est non scelerisque. Praesent vulputate blandit urna at convallis. Phasellus nulla nisi, mattis quis eros id, feugiat dapibus ante. In condimentum fringilla elit, eu sagittis ex pulvinar sed. Sed auctor lacus quis tellus congue pretium at id dolor. Suspendisse ultricies eu neque non porttitor. Sed vulputate turpis nec urna fringilla, nec fringilla neque accumsan. Aliquam leo velit, fermentum sed dolor nec, ultrices feugiat lacus. Nulla bibendum est id faucibus tempus.</p>
+        <p>Vivamus dictum commodo justo nec lacinia. Fusce ullamcorper ligula a semper aliquam. Donec cursus sem auctor lorem faucibus, non posuere est dignissim. Fusce tristique id magna vel maximus. Nam tincidunt gravida felis, non tristique neque varius a. Fusce orci urna, auctor in tortor id, rhoncus elementum libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis sollicitudin eros ut neque sollicitudin, non tincidunt leo auctor. Nam tincidunt fermentum scelerisque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer a nulla tempor, viverra justo sit amet, scelerisque ex.</p>
+      </article>
